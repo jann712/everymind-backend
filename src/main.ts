0f03ea14c8fad1fc7161203c86b62fb13714ab7f
jann712 <joao.augustonn1@gmail.com>
2024-01-17 16:13:40 -0300

    frontend url replacement

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import 'dotenv/config'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({origin: "http://localhost:5173"})
+  app.enableCors({origin: process.env.FRONTEND_URL})
   await app.listen(3000);
 }
 bootstrap();
